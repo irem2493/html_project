5e0ee3ed486ab34c201c7d066435dbeccfe4a922
irem2493 <57314574+irem2493@users.noreply.github.com>
2024-10-18 01:04:07 +0900

    autoLotto_commit

--- lotto.js
+++ lotto.js
@@ -6,7 +6,7 @@ let lottoBnt = document.querySelector("#lotto_number_btn");
 lottoBnt.addEventListener("click",setLotto);
 
 function setAutoLotto(){
-    for(let i = 0; i < 6; i++){
+    for(let i = 0; i < 7; i++){
         lottoAutoNumbers[i] = Math.floor(Math.random() * 45) + 1;
         for(let j = 0; j < i; j++){
             if(lottoAutoNumbers[j] == lottoAutoNumbers[i]) --i;
@@ -35,7 +35,7 @@ function showLottoAutoNumbers(){
 }
 const lottoNumbers = new Set();
 function setLotto(){
-   while(lottoNumbers.size < 6){
+   while(lottoNumbers.size < 7){
         let num = Math.floor(Math.random()*45)+1; //floor는 바닥이라는 뜻을 가짐, 내리겠다.
         lottoNumbers.add(num);
     }
@@ -55,7 +55,7 @@ function setLotto(){
     lottoBnt.addEventListener('mouseenter',function(){this.style.pointerEvents = 'none';});
 
     let resulLottoBnt = document.querySelector('#result_lotto_bnt');
-    resulLottoBnt.style.disply = 'block';
+    resulLottoBnt.style.display = 'block';
 }
 
 function showLottoNumbers(lottoNums){
@@ -72,30 +72,45 @@ resultLottoBnt.addEventListener("click", resultLotto);
 
 function resultLotto(){
     let lottoNums = Array.from(lottoNumbers);
+console.log(lottoNums);
+console.log(lottoAutoNumbers);
+
     let find, bnous;
     find = bnous = 0;
+    console.log(lottoNums.length);
+    console.log(lottoAutoNumbers.length);
     if(lottoNums.length > 0){
-        for(let i = 0; i < lottoNums.length-1; i++){
-            if(lottoNums[i] == lottoAutoNumbers[i]){
-                find++;
+        for(let i = 0; i < lottoAutoNumbers.length; i++){
+            for(let j = 0; j < lottoNums.length-1; j++){
+                if(lottoNums[i] === lottoAutoNumbers[j]) find++;
             }
         }
-        if(lottoNums[lottoNums.length-1] == lottoAutoNumbers[lottoNums.length-1])
-            bnous++;
+        for(let j = 0; j < lottoAutoNumbers.length; j++){
+            if(lottoNums[lottoNums.length-1] === lottoAutoNumbers[j]) bnous++;
+        }
     }
-    console.log(find);
-    console.log(bnous);
     showResult(find, bnous);
 }
 
 function showResult(find, bnous){
-    console.log(find + "--");
-    console.log(bnous + "--");
-    let result = document.querySelector('#result').style.disply = "block";
-    let list;
-    if(find < 3){
-        list = "<li><span>꽝</span>입니다.</li>";
-    }
+    console.log(find + "--f");
+    console.log(bnous + "--b");
+    let result = document.querySelector('#result');
+    result.style.disply = "block";
+    let lottoResultArea = document.querySelector('#lotto_result');
+    lottoResultArea.style.display = 'flex';
+    let list = "<ul>";
+    if(find === 3)  list += "<li><span>5등 당첨</span>입니다.</li>";
+    else if(find === 2 && bnous ===1) list += "<li><span>5등 당첨</span>입니다.</li>";
+    else if(find == 4) list += "<li><span>4등 당첨</span>입니다.</li>";
+    else if(find === 3 && bnous ===1) list += "<li><span>4등 당첨</span>입니다.</li>";
+    else if(find == 5) list = "<li><span>3등 당첨</span>입니다.</li>";
+    else if(find == 4 && bnous ===1) list += "<li><span>3등 당첨</span>입니다.</li>";
+    else if(find == 6) list += "<li><span>1등 당첨</span>입니다.</li>";
+    else if(find == 5 && bnous == 1) list += "<li><span>2등 당첨</span>입니다.</li>";
+    else list += "<li><span>꽝</span>입니다.</li>";
+    list += "</ul>";
+    console.log(list);
     let lottoResult = document.querySelector('#lotto_result');
     lottoResult.innerHTML = list;
 }
