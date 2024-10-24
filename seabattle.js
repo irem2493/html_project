a5b514afe3cb4654c584339e2e0b4c692333e54d
irem2493 <57314574+irem2493@users.noreply.github.com>
2024-10-24 22:31:41 +0900

    SeaBattle_commit

--- seabattle.js
+++ seabattle.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
     let $game_grid =$('.game_grid');
     for(let i = 1; i <= 100; i++){
-        const newGrid = `<div class ="grid-item" id = "${i}" ></div>`;
+        const newGrid = `<div class ="grid-item" id = ${i} ></div>`;
         $game_grid.append(newGrid);
+    }
+
+    let $alphabet = $('.alphabet');
+    for(let i = 0; i < 10; i++){
+        let letter = String.fromCharCode(65 + i); // 65 is the ASCII code for 'A'
+        const newAlph =  `<div class ="alphabet-item" id = "${letter}" >${letter}</div>`;
+        $alphabet.append(newAlph);
+    }
+
+    let $numbers = $('.numbers');
+    for(let i = 1; i <= 10; i++){
+        const newNum = `<div class ="number" id = ${i} >${i}</div>`;
+        $numbers.append(newNum);
     }
 });
